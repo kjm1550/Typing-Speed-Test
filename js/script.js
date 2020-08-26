b28e7c89728412ac943fa8080fa355ae965fa1d5
kjm1550 <kjm1550@gmail.com>
2020-08-25 22:29:32 -0500

    Added the Option to Change the Paragraph

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+const paragraph = document.querySelectorAll('.paragraph');
 const textArea = document.querySelector('#textbox');
 const result = document.querySelector('#results');
 const timer = document.querySelector('#timer');
 const testAgain = document.querySelector('#testAgain');
+const newText = document.querySelector('#newText');
 const timeButtons = document.querySelectorAll('.time-button');
 const defaultTimeButton = document.querySelector('#default-time-button');
 
@@ -11,6 +13,8 @@ let wpm = 0;
 let time = 60000;
 let endGame;
 let timerTimeOut;
+
+import { options } from './textOptions.js';
 
 function startTest() {
 	if (testStarted) return;
@@ -91,6 +95,11 @@ function resetTimeButtons() {
 	defaultTimeButton.disabled = true;
 }
 
+function changeParagraphs() {
+	paragraph.forEach((para) => (para.textContent = options[Math.floor(Math.random() * 6)]));
+}
+
 textArea.addEventListener('input', startTest);
 testAgain.addEventListener('click', restartTest);
+newText.addEventListener('click', changeParagraphs);
 timeButtons.forEach((timeButton) => timeButton.addEventListener('click', changeTime));
